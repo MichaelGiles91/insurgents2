f55ec35e7c147f8d9fc6ef68c2f2f399bfb3fad8
Giles <mrgiles@student.fullsail.edu>
2026-04-10 21:45:23 -0400

    hooked up UI, pause, lose, and win menus with button functionality. also hooked up lava damage to player.

---
+++ Assets/scripts/Door.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] GameObject model;
+    [SerializeField] GameObject button;
+
+    bool canOpen;
+
+
+  
+    void Update()
+    {
+        if(Input.GetButtonDown("Interact") && canOpen)
+        {
+            model.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<IOpen>() != null)
+        {
+            canOpen = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            model.SetActive(true);
+            canOpen = false;
+            button.SetActive(false);
+        }
+    }
+
+  
+}
---
+++ Assets/scripts/IPush.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public interface IPush
+{
+    public void getPushVel(Vector3 pushAmount);
+
+
+}
