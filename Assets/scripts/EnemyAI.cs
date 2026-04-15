72a5642864486a6e43f87011fe188ebad0c0ea72
alexariel96 <166575075+alexariel96@users.noreply.github.com>
2026-04-15 19:14:58 -0400

    Zombie added
    Enemy AI walks up to player and does damage

--- Assets/scripts/EnemyAI.cs
+++ Assets/scripts/EnemyAI.cs
@@ -1,31 +1,94 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour, IDamage
 {
+    [SerializeField] NavMeshAgent agent;    
     [SerializeField] Renderer model;
     [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int meleeDamage;
+    [SerializeField] float attackRate = 1f;
+    [SerializeField] float attackRange = 2f;
+
+    float attackTimer;
+    Transform player;
 
     Color colorOrig;
+    Vector3 playerDirection;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         colorOrig = model.material.color;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+      
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        attackTimer += Time.deltaTime;
+        playerDirection = (gameManager.instance.player.transform.position - transform.position);
+
+        agent.SetDestination(gameManager.instance.player.transform.position);
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            faceTarget();
+        }
+        if (distance <= attackRange && attackTimer >= attackRate)
+        {
+            attack();
+
+        }
+    }
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDirection.x, 0, playerDirection.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+
+
+    }
+
+    void attack()
+    {
+        attackTimer = 0;
+
+        IDamage damageable = player.GetComponent<IDamage>();
+
+        if (damageable != null)
+        {
+            damageable.takeDamage(meleeDamage);
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && attackTimer >= attackRate)
+        {
+            IDamage damageable = other.GetComponent<IDamage>();
+
+            if (damageable != null)
+            {
+                damageable.takeDamage(meleeDamage);
+                attackTimer = 0;
+                attack();
+            }
+        }
     }
     public void takeDamage(int amount)
     {
         HP -= amount;
 
-        if(HP <= 0)
+        if (HP <= 0)
         {
+            
             Destroy(gameObject);
         }
         else
