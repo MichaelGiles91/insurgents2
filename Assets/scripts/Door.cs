666da43db87da2c86db618dbe554cba96ddde97f
Giles <mrgiles@student.fullsail.edu>
2026-04-16 16:19:33 -0400

    worked on the doors they open to 90 degrees now, added target dummies for tutorial gun range.  hit the switch in each lane and the target will come out of the floor

--- Assets/scripts/Door.cs
+++ Assets/scripts/Door.cs
@@ -1,22 +1,64 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] GameObject model;
+    [SerializeField] Transform model;
     [SerializeField] GameObject button;
+    [SerializeField] TMP_Text closedText;
+   
+
+    [Header("Door Settings")]
+    [SerializeField] float openAngle = 90f;
+    [SerializeField] float speed = 2f;
 
     bool canOpen;
     bool isOpen;
 
+    Quaternion closedRotation;
+    Quaternion openRotation;
+
+    private void Start()
+    {
+        closedRotation = model.rotation;
+        openRotation = closedRotation * Quaternion.Euler(0f, openAngle, 0f);
+    }
 
 
-  
     void Update()
     {
-        if(Input.GetButtonDown("Interact") && canOpen)
+        if (gameManager.instance != null && gameManager.instance.isPaused)
         {
-            model.SetActive(false);
+            button.SetActive(false);
+            return;
+        }
+        if (Input.GetButtonDown("Interact") && canOpen)
+        {
+            isOpen = !isOpen;
+        }
+        if (canOpen)
+        {
+            if (isOpen)
+            {
+                closedText.text = "Press E to Close";
+            }
+            else
+            {
+                closedText.text = "Press E to Open";
+
+            }
+        }
+        if (isOpen)
+        {
+            model.rotation = Quaternion.Lerp(model.rotation, openRotation, speed * Time.deltaTime);
+
+        }
+        else
+        {
+            model.rotation = Quaternion.Lerp(model.rotation, closedRotation, speed * Time.deltaTime);
+
         }
     }
 
@@ -33,7 +75,6 @@ public class Door : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            model.SetActive(true);
             canOpen = false;
             button.SetActive(false);
         }
