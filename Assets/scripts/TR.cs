5d037f15c881c5c603167ff4ff6db31b10a83c73
DJCool360 <fuzzyhead5555@gmail.com>
2026-04-18 21:08:08 -0400

    more audio stuff
    struggling on some minors things so when someone is free to help cool

--- Assets/scripts/TR.cs
+++ Assets/scripts/TR.cs
@@ -14,9 +14,28 @@ public class TR : MonoBehaviour
     [SerializeField] float R3ET;
     [SerializeField] float EDST;
     [SerializeField] float EDET;
+    [SerializeField] float RI1ST;
+    [SerializeField] float RI2ST;
+    [SerializeField] float RI3ST;
+    [SerializeField] float RI1ET;
+    [SerializeField] float RI2ET;
+    [SerializeField] float RI3ET;
+    [SerializeField] float RC1st;
+    [SerializeField] float RC2ST;
+    [SerializeField] float RC3ST;
+    [SerializeField] float RC1ET;
+    [SerializeField] float RC2ET;
+    [SerializeField] float RC3ET;
 
     bool isRiddlePlaying;
     bool isPlayer;
+    bool RA1;
+    bool RA2;
+    bool RA3;
+    bool clock;
+    bool air;
+    bool candle;
+    bool selected;
     float Timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -82,7 +101,25 @@ public class TR : MonoBehaviour
             {
                 VO.Stop();
             }
+
+            if(clock == selected && RA1 == true)
+            {
+              VO.Play();
+            }
+
+            else
+            {
+              
+            }
+
         }
+    }
+
+    void RiddleAnswer()
+    {
+      clock = RA1;
+      air = RA2;
+      candle = RA3;
     }
 
  
