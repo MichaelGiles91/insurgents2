0413cf3954de075ff4d7d37f82a65a10e9974591
alexariel96 <166575075+alexariel96@users.noreply.github.com>
2026-04-19 21:56:19 -0400

    Update
    Cascading fixed

--- Assets/scripts/EnemyAI.cs
+++ Assets/scripts/EnemyAI.cs
@@ -16,7 +16,7 @@ public class EnemyAI : MonoBehaviour, IDamage
 
     float attackTimer;
     Transform player;
-
+    float distance;
     Color colorOrig;
     Vector3 playerDirection;
 
@@ -39,7 +39,7 @@ public class EnemyAI : MonoBehaviour, IDamage
 
         agent.SetDestination(gameManager.instance.player.transform.position);
 
-        float distance = Vector3.Distance(transform.position, player.position);
+         distance = Vector3.Distance(transform.position, player.position);
 
 
         if (agent.remainingDistance <= agent.stoppingDistance)
