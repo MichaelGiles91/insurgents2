325b8f39a466f1287c19dc8a6d610e526f5070cc
Giles <mrgiles@student.fullsail.edu>
2026-04-21 00:13:17 -0400

    put  guns, health pack and music is almost done

--- Assets/scripts/Damage.cs
+++ Assets/scripts/Damage.cs
@@ -25,7 +25,7 @@ public class Damage : MonoBehaviour
     {
         if(type == damageType.bullet)
         {
-           
+            rb.linearVelocity = transform.forward * speed;
             Destroy(gameObject, destroyTime);
         }
     }
