f02e5911ae64a49910ceedfbe39b6af38e87d957
Giles <mrgiles@student.fullsail.edu>
2026-04-22 19:36:46 -0400

    went through each scene and put a click audio sound for the menus for audio feedback

--- Assets/scripts/Button Functions.cs
+++ Assets/scripts/Button Functions.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,40 +7,72 @@ public class ButtonFunctions : MonoBehaviour
 {
     public GameObject settingsPanel;
     public GameObject PauseMenu;
+    public AudioSource aud;
+    public AudioClip clickSFX;
+
     public void StartGame()
     {
-        SceneManager.LoadScene("CentralHubScene");
+        aud.PlayOneShot(clickSFX);
+        StartCoroutine(LoadSceneSound("CentralHubScene"));
     }
     public void resume()
     {
+        aud.PlayOneShot(clickSFX);
         gameManager.instance.stateUnpause();
     }
 
     public void restart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        aud.PlayOneShot(clickSFX);
         gameManager.instance.stateUnpause();
+        StartCoroutine(LoadSceneSound(SceneManager.GetActiveScene().name));
+        
     }
 
     public void quit()
     {
-        SceneManager.LoadScene("HomeMenuScene");
+        aud.PlayOneShot(clickSFX);
+        StartCoroutine(LoadSceneSound("HomeMenuScene"));
     }
     public void respawnPlayer()
     {
+        aud.PlayOneShot(clickSFX);
         gameManager.instance.playerScript.spawnPlayer();
         gameManager.instance.stateUnpause();    
     }
     public void OpenSettings()
     {
-        Debug.Log("OpenSettings fired");
+        aud.PlayOneShot(clickSFX);
         PauseMenu.SetActive(false);
         settingsPanel.SetActive(true);
     }
     public void CloseSettings()
     {
-        
+        aud.PlayOneShot(clickSFX);
         settingsPanel.SetActive(false);
         PauseMenu.SetActive(true);
+    }
+    public void QuitGame()
+    {
+        StartCoroutine(QuitWithSound());
+
+    }
+    IEnumerator LoadSceneSound(string scene)
+    {
+        yield return new WaitForSecondsRealtime(0.2f);
+        SceneManager.LoadScene(scene);
+    }
+    IEnumerator QuitWithSound()
+    {
+        aud.PlayOneShot(clickSFX);
+
+        yield return new WaitForSecondsRealtime(0.2f);
+#if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#else 
+    Application.Quit();
+#endif
     }
 }
