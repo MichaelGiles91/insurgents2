105190e46ed4d597eb7512b28c37c33e25e707d9
alexariel96 <166575075+alexariel96@users.noreply.github.com>
2026-05-02 14:36:42 -0400

    Enemy AI
    Enemy zombie does not damage player, however they are able to roam and see the player once inside FOV

--- Assets/scripts/EnemyAI.cs
+++ Assets/scripts/EnemyAI.cs
@@ -6,24 +6,48 @@ public class EnemyAI : MonoBehaviour, IDamage
 {
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Renderer model;
+   
     [SerializeField] int HP;
     [SerializeField] int faceTargetSpeed;
+    [SerializeField] int FOV;
+    [SerializeField] Transform attackPOS;
+    [SerializeField] Transform headPOS;
+
     [SerializeField] int meleeDamage;
     [SerializeField] float attackRate = 1f;
     [SerializeField] float attackRange = 2f;
+
     [SerializeField] Animator animate;
     [SerializeField] int animateTransitionSpeed;
+   
 
     float attackTimer;
+    float angleToPlayer;
+
     Transform player;
     float distance;
     Color colorOrig;
     Vector3 playerDirection;
 
+    bool playerInTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        playerInTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        playerInTrigger = false;   
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    { 
+        //maybe we can add a game goal count here for finale prooduct.
         colorOrig = model.material.color;
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
@@ -32,30 +56,53 @@ public class EnemyAI : MonoBehaviour, IDamage
     // Update is called once per frame
     void Update()
     {
+        attackTimer += Time.deltaTime;
 
-        if(agent == null)
+        if (agent == null)
         {
             return;
         }
         locomotionAnim();
 
-        attackTimer += Time.deltaTime;
-        playerDirection = (gameManager.instance.player.transform.position - transform.position);
-
-        agent.SetDestination(gameManager.instance.player.transform.position);
-
-         distance = Vector3.Distance(transform.position, player.position);
-
-
-        if (agent.remainingDistance <= agent.stoppingDistance)
+        if (playerInTrigger && canSeePlayer())
         {
-            faceTarget();
-        }
-        if (distance <= attackRange && attackTimer >= attackRate)
-        {
-            attack();
 
+           
         }
+        
+    }
+
+    bool canSeePlayer()
+    {
+        playerDirection = (gameManager.instance.player.transform.position - headPOS.position); //where enemy is going minus player direction
+        angleToPlayer = Vector3.Angle(playerDirection, transform.forward);
+        Debug.DrawRay(headPOS.position, playerDirection);
+
+        
+        RaycastHit hit;
+        if (Physics.Raycast(headPOS.position,playerDirection,out hit))
+        {
+            if (angleToPlayer <= FOV && hit.collider.CompareTag("Player"))
+            {
+                agent.SetDestination(gameManager.instance.player.transform.position); // tells AI where enemy is
+
+                // = Vector3.Distance(transform.position, player.position);
+
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    faceTarget();
+                    attack();
+                }
+                //if (attackTimer >= attackRate)
+                //{
+                //    attack();
+
+                //}
+                return true;
+            }
+       
+        }
+        return false;
     }
     void locomotionAnim()
     {
@@ -69,7 +116,6 @@ public class EnemyAI : MonoBehaviour, IDamage
         Quaternion rot = Quaternion.LookRotation(new Vector3(playerDirection.x, 0, playerDirection.z));
         transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
 
-
     }
 
     void attack()
@@ -78,25 +124,26 @@ public class EnemyAI : MonoBehaviour, IDamage
 
         IDamage damageable = player.GetComponent<IDamage>();
 
-        if (damageable != null)
+        if (damageable != null && attackRange > 0)
         {
             damageable.takeDamage(meleeDamage);
+            
         }
     }
-    void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player") && attackTimer >= attackRate)
-        {
-            IDamage damageable = other.GetComponent<IDamage>();
+    //void OnTriggerStay(Collider other)
+    //{
+    //    if (other.CompareTag("Player") && attackTimer >= attackRate)
+    //    {
+    //        IDamage damageable = other.GetComponent<IDamage>();
 
-            if (damageable != null)
-            {
-                damageable.takeDamage(meleeDamage);
-                attackTimer = 0;
-                attack();
-            }
-        }
-    }
+    //        if (damageable != null)
+    //        {
+    //            damageable.takeDamage(meleeDamage);
+    //            attackTimer = 0;
+    //            attack();
+    //        }
+    //    }
+    //}
     public void takeDamage(int amount)
     {
         HP -= amount;
